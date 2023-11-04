90ea4da27902b189972ed46c6518ba9fe4f0a2c0
John Li (Tet) <tet@tet.moe>
2023-11-04 00:29:21 -0700

    change tiaa public EP

--- website/constants.tsx
+++ website/constants.tsx
@@ -1,2 +1,2 @@
 export const apiDomain =
-  process.env.NODE_ENV === "production" ? "https://tiaaapi.tet.moe/" : `http://localhost:443`;
+  process.env.NODE_ENV === "production" ? "https://tiaaapi.tet.moe" : `http://localhost:443`;
