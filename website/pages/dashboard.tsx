7750bda60f6264ee833e1e4da1a79d1be6bf3a93
John Li (Tet) <tet@tet.moe>
2023-11-10 01:13:35 -0700

    dashboard redirects

--- website/pages/dashboard.tsx
+++ website/pages/dashboard.tsx
@@ -1,7 +1,13 @@
 import exp from "constants";
 import { SidebarLayout } from "../components/SidebarLayout";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export const DashboardPage = () => {
+  const router = useRouter();
+  useEffect(()=>{
+    router.push("/advisor");
+  },[])
   return (
     <SidebarLayout title={"r"}>
       <div className={`flex flex-col gap-8 p-8`}>
